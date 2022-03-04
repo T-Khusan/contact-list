ec5348ff3bac72b57eb57eb8c66154a0f32fca98
Khusan Tojiboev <khusan.tojiboev@gmail.com>
2022-03-04 12:05:41 +0500

    Migrated table

--- user_service/migrations/1_create_table.up.sql
+++ user_service/migrations/1_create_table.up.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS users (
   id uuid primary key DEFAULT (uuid_generate_v4()),
   name varchar(150) not null,
   lastname varchar(150),
