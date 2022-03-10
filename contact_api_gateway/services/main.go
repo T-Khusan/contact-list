2b9b1bf2bf57ba87653e63242fdc1a9541e0fb01
Khusan Tojiboev <khusan.tojiboev@gmail.com>
2022-03-10 13:03:01 +0500

    Connected as client to contact_service

--- contact_api_gateway/services/main.go
+++ contact_api_gateway/services/main.go
@@ -1,34 +1,34 @@
 package services
 
 import (
+	"contact_api_gateway/config"
+	"contact_api_gateway/genproto/user_service"
 	"fmt"
 
-	"bitbucket.org/udevs/example_api_gateway/config"
 	"google.golang.org/grpc"
 )
 
 type ServiceManager interface {
-	// ProfessionService() position_service.ProfessionServiceClient
-	
+	UserService() user_service.UserServiceClient
 }
 
 type grpcClients struct {
-	// professionService position_service.ProfessionServiceClient
+	userService user_service.UserServiceClient
 }
 
 func NewGrpcClients(conf *config.Config) (ServiceManager, error) {
-	connPositionService, err := grpc.Dial(
-		fmt.Sprintf("%s:%d", conf.PositionServiceHost, conf.PositionServicePort),
+	connUserService, err := grpc.Dial(
+		fmt.Sprintf("%s:%d", conf.UserServiceHost, conf.UserServicePort),
 		grpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
 
 	return &grpcClients{
-		// professionService: position_service.NewProfessionServiceClient(connPositionService),
+		userService: user_service.NewUserServiceClient(connUserService),
 	}, nil
 }
 
-// func (g *grpcClients) ProfessionService() position_service.ProfessionServiceClient {
-// 	return g.professionService
-// }
+func (g *grpcClients) UserService() user_service.UserServiceClient {
+	return g.userService
+}
